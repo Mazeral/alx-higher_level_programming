0b258a364601d36e821e327229a6bef1a03d6458
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-01-03 21:15:25 +0200

    Print_comb3.

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if (i * 10 + j) == 89:
-            print("{:02}".format(i * 10 +j))
+            print("{:02}".format(i * 10 + j))
         else:
             print("{:02}, ".format(i * 10 + j), end='')
