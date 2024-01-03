d8b34db57cfbb1601d8bac41177fa3d5e644c0c4
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-01-03 20:30:27 +0200

    Print comb2.

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,4 @@ for i in range(100):
     if i == 99:
         print(i)
     elif i < 10:
-        print("0{}, ".format(i), end='')
-    else:
-        print("{}, ".format(i), end='')
+        print(f"{i}:2d, ", end='')
