298cc861f4d65a849a17bdc9ccd6d29eecad01f1
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-01-03 23:06:40 +0200

    pycode errors

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print("{}".format(number % 10), end='')
+    if number < 0:
+        print("{}".format(-number % 10), end='')
+    else:
+        print("{}".format(number % 10), end='')
     return (number % 10)
