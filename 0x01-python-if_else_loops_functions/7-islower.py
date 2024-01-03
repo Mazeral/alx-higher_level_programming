f716b9de713eada8ebcb9526ece6433258fb0061
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-01-03 21:23:33 +0200

    islower.

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
     if int(ord(c)) < 97:
-        print("upper")
+        print("{} is upper".format(c))
     else:
-        print("lower")
+        print("{} is lower".format(c))
