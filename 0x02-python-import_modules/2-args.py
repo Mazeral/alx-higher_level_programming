dd0b8cd46213b8e6966afbb4eea7f166e7b061a7
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-01-04 09:45:12 +0200

    Solved the fourth task!

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+if (len(sys.argv) - 1) == 0:
+    print("0 arguments.")
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
+    count = 0
+    for i in sys.argv[1:]:
+        print("{}: {}".format(count, i))
+        count+=1
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import sys
+sum = 0
+for i in sys.argv[1:]:
+    sum+=int(i)
+print(sum)
