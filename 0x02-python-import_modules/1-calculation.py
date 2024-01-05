50791c036eb64caeb730c62cc4a50773ce4bf3ae
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-01-05 11:24:55 +0200

    Fixing th esolutions.

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 import calculator_1
 a = 10
 b = 5
-print(add(a, b))
-print(sub(a, b))
-print(mul(a, b))
-print(div(a, b))
+print("{} + {} = {}".format(a, b, calculator_1.add(a,b)))
+print("{} - {} = {}".format(a, b, calculator_1.sub(a,b)))
+print("{} * {} = {}".format(a, b, calculator_1.mul(a,b)))
+print("{} / {} = {}".format(a, b, calculator_1.div(a,b)))
