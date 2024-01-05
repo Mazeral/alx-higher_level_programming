a864351525d6ae2e785f93bc8b7d2c74c1dbc22c
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-01-05 19:00:04 +0200

    Fixing things8.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         print("1 argument:\n1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
-        count = 0
+        count = 1
         for i in sys.argv[1:]:
             print("{}: {}".format(count, i))
             count += 1
