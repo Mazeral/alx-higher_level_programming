da860458568d44ea03f60e8980b319fc70c77354
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-01-05 11:27:11 +0200

    Fixing th esolutions2.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,9 @@
 import sys
 if (len(sys.argv) - 1) == 0:
     print("0 arguments.")
+elif len(sys.argv) - 1 == 1:
+
+        print("1 argument:\n1: {}".format(sys.argv[1]))
 else:
     print("{} arguments:".format(len(sys.argv) - 1))
     count = 0
