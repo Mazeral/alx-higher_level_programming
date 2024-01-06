dc1af97f7395325330b3d2e9498f7bc914832aad
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-01-06 11:24:21 +0200

    Element at task.

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        for i in my_list:
-            print("{}".format(str.format(str(i))))
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        answer = "{}"
+        print(answer.format(i))
