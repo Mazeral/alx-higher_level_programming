cb808c94b4624476afefcce573db3f0ca3322a8f
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-01-07 20:21:30 +0200

    Last mandetory.

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * is_palindrome - a function to check if the linked list is palindrome
+ * @head: The head, AKA the start of the linked list
+ * Return: 0 if not, 1 if yes
+ */
+int is_palindrome(listint_t **head)
+{
+	size_t len = 0, i;
+	listint_t *node = *head;
+	int *arr;
+
+	while (node != NULL)
+	{
+		len++;
+		node = node->next;
+	}
+	if (len == 0)
+		return (0);
+
+	arr = malloc(sizeof(int) * len);
+	i = 0;
+
+	while (node != NULL)
+	{
+		arr[i] = node->n;
+		i++;
+		node = node->next;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		if (arr[i] != arr[len - i])
+			return (1);
+	}
+
+	return (0);
+}
