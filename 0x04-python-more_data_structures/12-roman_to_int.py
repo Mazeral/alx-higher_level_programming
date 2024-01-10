5cd72d0f9f7990d0c69c636f59ef0debf9da1541
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-01-10 22:19:25 +0200

    12th task.

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    total = 0
+    for i in roman_string:
+        if i == 'I':
+            total = total + 1
+        elif i == 'V':
+            total = total + 5
+        elif i == 'X':
+            total = total + 10
+        elif i == 'L':
+            total = total + 50
+        elif i == 'C':
+            total = total + 100
+        elif i == 'D':
+            total = total + 500
+        elif i == 'M':
+            total = total + 1000
+    return total
