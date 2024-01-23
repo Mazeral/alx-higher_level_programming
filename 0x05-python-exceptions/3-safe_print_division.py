0a3481bedc92a253fe01d0c0d24a0e3882d6066e
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-01-23 21:16:12 +0200

    fourth task.

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+        return result
+    except (ZeroDivisionError, ValueError):
+        pass
+        result = None
+    finally:
+        if result == None:
+            print("Inside result: None")
+        else:
+            print("Inside result: {}".format(result))
