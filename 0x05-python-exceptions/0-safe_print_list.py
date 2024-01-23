434debec3c94a2308c11c8d32a72a3b8476945b4
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-01-23 18:46:25 +0200

    First task.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,10 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print(my_list[i], end='')
-            count+=1
-        print("")
-    except (IndexError, TypeError):
+            if i < len(my_list):
+                print(my_list[i], end=" ")
+                count += 1
+        print()
+    except Exception as e:
         pass
     return count
