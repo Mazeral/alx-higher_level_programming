d4eaa2df11da6effeab532ae849b4071d0c693d7
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-01-23 20:35:19 +0200

    fixing style.

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,9 +2,10 @@
 def safe_print_integer(value):
     value_cpy = str(value)
     try:
-        if value_cpy.isdigit() or (value_cpy.startswith('-') and value_cpy[1:].isdigit()):
-                print("{:d}".format(value))
-                return True
+        if value_cpy.isdigit() or \
+                (value_cpy.startswith('-') and value_cpy[1:].isdigit()):
+            print("{:d}".format(value))
+            return True
         else:
             return False
     except Exception:
