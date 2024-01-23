9d29474c9490f7a9a42707b2d2c40954cdc46679
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-01-23 20:33:24 +0200

    second task.

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    value_cpy = str(value)
+    try:
+        if value_cpy.isdigit() or (value_cpy.startswith('-') and value_cpy[1:].isdigit()):
+                print("{:d}".format(value))
+                return True
+        else:
+            return False
+    except Exception:
+        return False
