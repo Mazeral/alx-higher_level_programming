fd9aa71170dbe112ddb623c81f4de84fd4f02aee
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-01-25 09:53:05 +0200

    third task.

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Square class
+
+A basic class
+"""
+
+
+class Square():
+    """An empty Square class"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            print("size must be an integer", end='')
+            raise TypeError
+        if size < 0:
+            print("size must be >= 0", end='')
+            raise ValueError
+        self.__size = size
