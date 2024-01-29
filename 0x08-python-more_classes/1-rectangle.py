655f77d032a5cb726777e8f25d02b05f6e2d16e3
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-01-29 13:41:00 +0200

    fixing the second task.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,15 +6,22 @@ Rectangle class with some defined attributes.
 
 class Rectangle:
     """Rectangle class the basic backbone of a class"""
-    def __init__(self, width):
+    def __init__(self, width=0, height=0):
         self.__width = width
+        self.__height = height
 
     @property
     def width(self):
         """The width property."""
         return self.__width
-
     @width.setter
     def width(self, value):
         self.__width = value
-
+    
+    @property
+    def height(self):
+        """The height property."""
+        return self.__height
+    @height.setter
+    def height(self, value):
+        self.__height = value
