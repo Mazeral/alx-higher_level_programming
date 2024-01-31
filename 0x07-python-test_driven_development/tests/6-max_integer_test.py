90cdd2c47c515956f798398e3a2f3b75cd2891e2
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-01-31 20:34:17 +0200

    Fixing importing issue.

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,12 +6,12 @@ import unittest
 max_integer = __import__("6-max_integer").max_integer
 
 
+class TestMaxInteger(unittest.TestCase):
 """TestMaxInteger
 A unit test for testing max_integer function
 """
-class TestMaxInteger(unittest.TestCase):
-
     def test_max(self):
+        """a test with regular use cases, should be successful"""
         self.assertEqual(max_integer([5,4,3,2,1]), 5)
         self.assertEqual(max_integer([5,4,3,2,7]), 7)
         self.assertEqual(max_integer([5,4,100,2,1]), 100)
