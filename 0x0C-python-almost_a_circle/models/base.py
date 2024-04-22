6fb355e440351aeac287dc341f3ce31554bb7b68
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-04-22 10:37:31 +0200

    17th task done./15-main.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """The base class for the rest of the project"""
+import json
 
 class Base:
     """Class base
@@ -27,4 +28,26 @@ class Base:
             self.id = self.__nb_objects + 1
             Base.__nb_objects = Base.__nb_objects + 1
 
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        return json.dumps(list_dictionaries)
 
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON serialization of a list of objects to a file.
+
+        Args:
+            list_objs (list): A list of inherited Base instances.
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        return json.loads(json_string)
