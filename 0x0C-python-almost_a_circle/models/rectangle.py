43e0dddb1af84ae4d8c4a64db72af46d9e821ad8
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-04-22 06:33:59 +0200

    finished the 8th task.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,3 +75,30 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def display(self):
+        for vertical_offset in range(self.__y):
+            print("")
+        for x_display in range(self.__height):
+            for horizontal_offset in range(self.__x):
+                print(" ", end="")
+            for y_display in range(self.__width):
+                print('#', end="")
+            print("")
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args):
+        if len(args) > 0 and args[0]:
+            self.id = args[0]
+        if len(args) > 1 and args[1]:
+            self.__width = args[1]
+        if len(args) > 2 and args[2]:
+            self.__height = args[2]
+        if len(args) > 3 and args[3]:
+            self.__x = args[3]
+        if len(args) > 4 and args[4]:
+            self.__y = args[4]
