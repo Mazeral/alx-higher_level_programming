2ef6bfd941f30e5e82d43755345281fecf6578fa
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-05-21 16:58:47 +0200

    14th SQL Task.

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,2 +1,2 @@
 -- A script that removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT score, name FROM second_table WHERE score <= 5 ORDER BY score DESC;
+DELETE FROM second_table WHERE score <= 5;
