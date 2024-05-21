74c2fcae53435095897ff924d6ff4e51d3c17482
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-05-21 15:37:42 +0200

    4th SQL Task.

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,6 @@
 -- Shows all the tables from a database name provided by the user
-SHOW TABLES FROM '$value';
+-- Declaring a variable for the databse name
+@dbname = ?;
+
+-- Show the tables from a database
+SHOW TABLES FROM @dbname;
