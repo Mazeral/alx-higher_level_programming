811632fd3b70ab26f5af0d19f37e2e3aa27b2fbf
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-05-21 16:28:18 +0200

    10th SQL Task.

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,16 +1,10 @@
---  A script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
-CREATE table IF NOT EXISTS second_table (
-	`id` INT,
-	`name` VARCHAR(256),
-	`score` INT
-);
--- create these records:
---     id = 1, name = “John”, score = 10
---     id = 2, name = “Alex”, score = 3
---     id = 3, name = “Bob”, score = 14
---     id = 4, name = “George”, score = 8
-INSERT INTO second_table (id, name, score)
-VALUES (1, 'John', 10),
-	(2, 'Alex', 3),
-	(3, 'Bob', 14),
-	(3, 'George', 8);
+-- creates table second_table in the database hbtn_0c_0
+CREATE table IF NOT EXISTS second_table (`id` INT,
+`name` VARCHAR(256),
+`score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`)
+VALUES
+       (1, 'John', 10),
+       (2, 'Alex', 3),
+       (3, 'Bob', 14),
+       (4, 'George', 8);
