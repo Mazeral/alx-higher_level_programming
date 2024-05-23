311bb34d73e272c5d8dd7766a7647fbafea2c8da
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-05-23 02:45:40 +0200

    task 4

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,5 +5,5 @@ CREATE TABLE cities (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	state_it INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY(state_it) REFERENCES states(id)
+	FOREIGN KEY(state_id) REFERENCES states(id)
 );
