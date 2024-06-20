c30d2a79cc5556ef587f9a6d36f9b079ce8684b5
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-20 13:47:37 +0200

    9th task.

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const texts = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let index = 0; index < texts.length; index++) {
+	const element = texts[index];
+	console.log(element);
+}
---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/snap/bin/node
+// #!/usr/bin/node
+const size = parseInt(process.argv[2]);
+let output = "";
+if (!size)
+	console.log("Missing size");
+else
+{
+	for (let index = 0; index < size; index++) {
+		for (let index = 0; index < size; index++) {
+			output += "#"
+		}
+		if ((index + 1) < size)
+			output += "\n";
+	}
+	console.log(output)
+}
