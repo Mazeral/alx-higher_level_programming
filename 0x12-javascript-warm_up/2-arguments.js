fae2ab0de0a14b5805f7186bb4bb5bca22e2248a
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-21 02:04:08 +0200

    Completed 10th task.

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,7 @@
 if (process.argv.slice(2).length === 0) {
   console.log('No argument');
 } else {
+  if (process.argv.slice(2).length === 1) { console.log('Argument found'); }
 
-if (process.argv.slice(2).length === 1)
-	console.log("Argument found")
-
-if (process.argv.slice(2).length > 1)
-	console.log("Arguments found")
+  if (process.argv.slice(2).length > 1) { console.log('Arguments found'); }
 }
