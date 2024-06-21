3323c21419cfd5766bf2fb56689932a45fcf3d8f
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-22 01:53:04 +0200

    3rd task.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    A script that lists all the states 
+    that match the argument
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE\
+    '{}' ORDER BY id ASC".format(sys.argv[4])
+    cur.execute(query)
+    results = cur.fetchall()
+    for row in results:
+        print(row)
+    cur.close()
+    db.close()
