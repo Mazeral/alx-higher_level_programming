d599d2905d48999f5b17acf9f151e0118255925b
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-22 01:33:10 +0200

    first task.

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,20 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
+"""
+    A script that lists all the states and sortes
+    them by id
+"""
+
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                     passwd=sys.argv[2], db=sys.argv[3])
-cur = db.cursor()
-results = cur.execute("""SELECT name FROM states ORDER BY id""")
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("""SELECT name FROM states ORDER BY id""")
+    results = cur.fetchall()
+    for row in results:
+        print(row)
