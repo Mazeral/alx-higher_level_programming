57e0910d29499ea4e8e5ce2a192af66a0488c572
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-21 02:42:08 +0200

    4th task fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,20 +8,17 @@ class Rectangle {
   }
 
   print () {
-	if (this.width > 0 && this.height > 0)
-	{
-let seq = '';
-for (let index = 0; index < this.height; index++) {
-  for (let index = 0; index < this.width; index++) {
-    seq += 'X';
+    if (this.width > 0 && this.height > 0) {
+      let seq = '';
+      for (let index = 0; index < this.height; index++) {
+        for (let index = 0; index < this.width; index++) {
+          seq += 'X';
+        }
+        if (index + 1 < this.height) { seq += '\n'; }
+      }
+      console.log(seq);
+    }
   }
-    if (index + 1 < this.height)
-  seq += "\n";
-}
-console.log(seq);
-}
-
-		}
 }
 
 module.exports = Rectangle;
