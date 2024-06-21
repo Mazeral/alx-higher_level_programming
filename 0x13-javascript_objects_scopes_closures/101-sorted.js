f6451ca55d3a2acaf0b420155033186ba8b8bf82
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-21 04:47:59 +0200

    12th task fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,21 +3,11 @@ const data = require('./101-data.js').dict;
 
 function transformObject (obj) {
   const transformed = {};
-
-  // Iterate over each key in the original object
   Object.keys(obj).forEach(key => {
-    // Get the value corresponding to the current key
     const value = obj[key];
-
-    // If the value is not yet in the transformed object, initialize it as an empty array
-    if (!transformed[value]) {
-      transformed[value] = [];
-    }
-
-    // Push the current key into the array corresponding to its value
+    if (!transformed[value]) { transformed[value] = []; }
     transformed[value].push(key);
   });
-
   return transformed;
 }
 const dict = transformObject(data);
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,3 +4,4 @@ const source2 = process.argv[3];
 const dest = process.argv[4];
 const src1 = readFile(source1);
 const src2 = readFile(source2);
+const result
