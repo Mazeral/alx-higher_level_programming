233c9559e5dacbbcb905381bf8d9219f42e34cd8
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-22 01:19:32 +0200

    first task.

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,4 +5,4 @@ import sys
 db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                      passwd=sys.argv[2], db=sys.argv[3])
 cur = db.cursor()
-cur.execute("SELECT name FROM states ORDER BY id")
+results = cur.execute("SELECT name FROM states ORDER BY id")
