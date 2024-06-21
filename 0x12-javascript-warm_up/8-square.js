9453274c21bd9165df7c959fa1202fe1bcfbc42d
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-21 02:14:17 +0200

    Completed 10th task.

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const size = parseInt(process.argv[2]);
 let output = '';
-if (!size) { console.log("Missing size"); } else {
+if (!size) { console.log('Missing size'); } else {
   for (let index = 0; index < size; index++) {
     for (let index = 0; index < size; index++) {
       output += '#';
