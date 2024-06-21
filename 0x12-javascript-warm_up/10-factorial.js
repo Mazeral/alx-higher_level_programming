0e2d612788266246f34697d72a1f50ca041ec49c
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-21 02:18:28 +0200

    Completed 10th task.

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 function factorial (num) {
-  if (num <= 0) { return 1; }
+  if (num <= 0 || isNaN(num)) { return 1; }
   return num * factorial(num - 1);
 }
 console.log(factorial(parseInt(process.argv[2])));
