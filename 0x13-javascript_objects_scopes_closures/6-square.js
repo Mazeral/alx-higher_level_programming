60642f10b78f52a56ddf9b26f088cb7444863c1f
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-21 02:47:07 +0200

    7th task fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,51 +1,54 @@
-#!/snap/bin/node
-// #!/usr/bin/node
+#!/usr/bin/node
 class Rectangle {
-	constructor(w, h){
-		if (w > 0 && h > 0)
-		{
-			this.width = w;
-			this.height = h;
-		}
-	}
-	print(){
-		let seq = "";
-		for (let index = 0; index < this.height; index++) {
-			for (let index = 0; index < this.width; index++) {
-				seq += "#"	
-			}
-			seq += ""
-		}
-		console.log(seq);
-	}
-	rotate(){
-		const tmp = this.width;
-		this.width = this.height;
-		this.height = tmp;
-	}
-	double()
-	{
-		this.height = this.height * 2;
-		this.width = this.width * 2;
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-};
+  print () {
+    if (this.width > 0 && this.height > 0) {
+      let seq = '';
+      for (let index = 0; index < this.height; index++) {
+        for (let index = 0; index < this.width; index++) {
+          seq += 'X';
+        }
+        if (index + 1 < this.height) { seq += '\n'; }
+      }
+      console.log(seq);
+    }
+  }
 
-class Square extends Rectangle{
-	constructor(size)
-	{
-		super(size,size);
-	}
-	charPrint(character){
-		if (!character)
-			character = "X"
-		let seq = "";
-		for (let index = 0; index < this.height; index++) {
-			for (let index = 0; index < this.width; index++) {
-				seq += character	
-			}
-			seq += ""
-		}
-		console.log(seq);
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
 }
-module.exports.Square = Square;
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (character) {
+    if (this.height > 0) {
+      if (!character) { character = 'X'; }
+      let seq = '';
+      for (let index = 0; index < this.height; index++) {
+        for (let index = 0; index < this.width; index++) {
+          seq += character;
+        }
+        seq += '';
+      }
+      console.log(seq);
+    }
+  }
+}
+module.exports = Square;
