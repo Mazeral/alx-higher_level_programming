684e19b3a6be90541b7150ff89ef5a5734d8261f
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-21 03:08:59 +0200

    9th task fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@ exports.esrever = function (list) {
   const size = list.length;
   const reversed = [];
   for (let index = size - 1; index > -1; index--) {
-    reversed.append(list[index]);
+    reversed.push(list[index]);
   }
   return reversed;
 };
