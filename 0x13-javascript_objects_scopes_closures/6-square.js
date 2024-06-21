950c3a97e2e36359a6a4b218f60e59295376b795
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-21 03:03:49 +0200

    7th task fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,38 +1,5 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    if (this.width > 0 && this.height > 0) {
-      let seq = '';
-      for (let index = 0; index < this.height; index++) {
-        for (let index = 0; index < this.width; index++) {
-          seq += 'X';
-        }
-        if (index + 1 < this.height) { seq += '\n'; }
-      }
-      console.log(seq);
-    }
-  }
-
-  rotate () {
-    const tmp = this.width;
-    this.width = this.height;
-    this.height = tmp;
-  }
-
-  double () {
-    this.height = this.height * 2;
-    this.width = this.width * 2;
-  }
-}
-
-class Square extends Rectangle {
+class Square extends require('./5-square') {
   constructor (size) {
     super(size, size);
   }
