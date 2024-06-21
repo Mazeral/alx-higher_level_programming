c8d77100a92d9589bfe38c4a4099079f10737c14
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-21 02:20:17 +0200

    Fixed 11th task.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const arr = process.argv.slice(2);
+if (arr.length < 2) console.log(0);
 const numarr = arr.map(str => parseInt(str, 10));
 const sorted = numarr.sort((a, b) => a - b);
 console.log(numarr[sorted.length - 2]);
