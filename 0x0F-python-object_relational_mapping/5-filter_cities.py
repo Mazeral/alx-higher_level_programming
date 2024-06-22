666ef14ad0c189d3f61a3662f34159dc49f2f54b
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-22 21:39:45 +0200

    6th task

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-    A script that lists all the states
-    that match the argument of the user
+    a script that takes in the name of a state as an argument
+    and lists all cities of that state,
+    using the database hbtn_0e_4_usa
 """
 
 import MySQLdb
@@ -14,9 +15,11 @@ if __name__ == "__main__":
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    query = """SELECT id, name, state\
-    FROM states WHERE name LIKE BINARY\
-    %s ORDER BY id ASC"""
+    # First, you need to join the 2 tables on a common key
+    # After that, you will select the table you want to fetch the data from
+    # Finally, select the data (Remember that this is how SQL works!)
+    query = """SELECT cities.name FROM cities\
+            JOIN states ON cities.state_id = states.id"""
     cur.execute(query, (sys.argv[4],))
     results = cur.fetchall()
     for row in results:
