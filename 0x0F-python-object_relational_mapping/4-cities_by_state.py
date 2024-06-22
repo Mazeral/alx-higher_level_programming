c3804528b599df6a32f1cffdefc36097fe492c57
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-22 03:03:22 +0200

    5th task.

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    A script that lists all the states
+    that match the argument of the user
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    query = """SELECT id, name, state\
+    FROM states WHERE name LIKE BINARY\
+    %s ORDER BY id ASC"""
+    cur.execute(query)
+    results = cur.fetchall()
+    for row in results:
+        print(row)
+    cur.close()
+    db.close()
