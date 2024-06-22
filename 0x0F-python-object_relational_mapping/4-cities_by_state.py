680e72a732d4695d4ce75c8f0310a25e662e2da5
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-23 01:44:41 +0200

    fixing the 2nd task

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    query = """SELECT id, name, state\
+    query = """SELECT cities.id, cities.name, states.name\
     FROM cities JOIN states ON cities.state_id = states.id
     ORDER BY cities.id ASC"""
     cur.execute(query, (sys.argv[4],))
