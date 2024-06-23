e1b757fbf65de676dd175baea392e2267a3f8018
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-23 03:26:02 +0200

    9th task

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     if states is None:
         print("Nothing")
     # Print each state
-    else states:
+    else:
         print("{}: {}".format(states.id, states.name))
 
     session.close()
