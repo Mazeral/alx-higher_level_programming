4b52ae9889c0fe18c5258865a045a6becec081ac
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-23 03:22:48 +0200

    9th task

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This script fetches all states from the database and prints them
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    # Create a session to interact with the database
+    session = Session()
+
+    # Fetch all states from the database and order them by id
+    states = session.query(State).order_by(State.id).first()
+
+    if not states:
+        print("Nothing")
+    # Print each state
+    if states:
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
+
+    session.close()
