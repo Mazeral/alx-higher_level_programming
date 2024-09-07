6049bcc714a919a3d13096d58068f793af585da5
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-09-07 15:59:07 +0200

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,7 +3,8 @@
 a POST request to http://0.0.0.0:5000/search_user
 with the letter as a parameter."""
 
-import sys, requests
+import sys
+import requests
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
     if len(sys.argv) > 1:
