76aeb838af6636b35d65c82284d2525cfe5713fc
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-09-07 14:44:55 +0200

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,6 +9,6 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = { "email" : sys.argv[2] }
+    email = {"email": sys.argv[2]}
     res = requests.post(url, data=email)
     print(res.content.decode())
