3834423cfa43cace25cde99c2dbb3abe84e9a4cb
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-09-07 15:56:59 +0200

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a Python script that takes in a letter and sends
+a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter."""
+
+import sys, requests
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q = sys.argv[1]
+    if q is None:
+        q = ""
+    resp = requests.post(url=url, data=q)
+    if not json.dump(resp):
+        print("Not a valid JSON")
+    elif json.dump(resp) and len(json.dump(resp)) == 0:
+        print("No result")
