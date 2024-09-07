c0a75f0c77e4c665413fcd3aa9a37811a9d4dda2
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-09-07 11:57:39 +0200

    task 2.

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" a Python script that takes in a URL and an email,
+fends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+sys will be used to receive the url and the mail
+"""
+
+from urllib import request
+import sys
+
+if __name__ == "__main__":
+    with urlopen(sys.argv[1], sys.argv[2]) as url, mail:
+        req = request.Request(url, mail)
+        resp = request.urlopen(req)
