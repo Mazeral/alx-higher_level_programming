4b51b31312882f5e744c66b9fa4e2af20f6f9ecb
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-09-07 14:19:25 +0200

    task 4 done.

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ import requests
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     resp = requests.get(url)
-    print("Body reponse:\n\t- type: {}\n\t- content: {}".\
+    print("Body reponse:\n\t- type: {}\n\t- content: {}".
           format(type(resp.content.decode()), resp.content.decode()))
