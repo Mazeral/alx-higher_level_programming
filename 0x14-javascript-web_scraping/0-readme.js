5bde68f731525992ec111f2b3b2013f54e829e29
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-09-08 22:27:35 +0200

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,12 +2,12 @@
 
 const fs = require('fs');
 
-console.log(process.argv[1]);
-fs.readFile(process.argv[1], 'utf8', (err, data) => {
+const path = process.argv[1]
+fs.readFile(path, 'utf8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    const decoder = new TextDecoder('utf-8');
+    const decoder = new TextDecoder('utf8');
     const fileText = decoder.decode(data);
     console.log(fileText);
   }
