55a6b6bbdf024f2e0f13212019c4535557bd0ca5
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-09-29 09:44:33 +0200

    styling task0.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """A function to read a utf-8 file"""
 
+
 def read_file(filename=""):
     """
     Reads a UTF-8 encoded file and prints its content.
 
     Args:
-        filename (str): The name of the file to read. Defaults to an empty string.
+        filename (str): The name of the file to read.
+        Defaults to an empty string.
 
     Returns:
         None
