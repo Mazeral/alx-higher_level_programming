0df15c9d712fcecd78be793813a3d87484b7e433
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-09-29 09:37:21 +0200

    task 1.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
+    """
+    Reads a UTF-8 encoded file and prints its content.
+
+    Args:
+        filename (str): The name of the file to read. Defaults to an empty string.
+
+    Returns:
+        None
+    """
     with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
         print(content)
